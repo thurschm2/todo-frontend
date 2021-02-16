36723ee7d29518aa3bb3af67f18c6ff8e48271de
thurschm2 <thurschm2@gmail.com>
2021-02-16 03:55:12 +0000

    first REACT implementation

---
+++ src/components/Task/Task.js
@@ -0,0 +1,14 @@
+import './Task.css';
+
+function Task(props) {
+    return (
+        <li className="task">
+            <p className="task__text">This is a task that needs to be done</p>
+            {props.complete === false && <button className="button">Complete</button>}
+            <button className="button">Delete</button>
+        </li>
+
+    );
+}
+
+export default Task;
